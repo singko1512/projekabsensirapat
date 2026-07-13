3fa0cde7e8d7d3870c687eded91dce13478b0975
singko1512 <adityadwiputranto943@gmail.com>
2026-07-13 10:00:53 +0700

    buat migration, model, controller, dan routes admin
    Punya admin

---
+++ app/Models/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable; 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+
+#[Fillable(['username', 'password', 'nama'])]
+#[Hidden(['password'])]
+class Admin extends Authenticatable
+{
+    use HasFactory;
+
+    protected $table = 'admins';
+    protected $primaryKey = 'id_admin';
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
+}
---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Admin::create([
+            'username' => 'admin_utama',
+            'nama'     => 'Administrator Sistem',
+            'password' => Hash::make('rahasia123'), // Ini password Anda
+        ]);
+    }
+}
